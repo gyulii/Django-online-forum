a3fb2f16924327fba8d12c693684db2096088e1f
gyulii <faragogyulii@gmail.com>
2024-05-03 13:01:40 +0200

    Small bug fix on the deployment and Ascii check removed

--- entry-point.sh
+++ entry-point.sh
@@ -2,7 +2,7 @@
 
 make forum
 
-DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD 
+export DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD 
 
 python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 python manage.py migrate --no-input
